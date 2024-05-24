da267004e007c1d14c84d9d075e2df01410ce462
Zlin <2768228421@qq.com>
2024-05-24 23:31:20 +0800

    csv 的读取和查看

---
+++ Study/Pytorch/csv文件生成与查看.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+os.makedirs(os.path.join('..','data'),exist_ok=True)
+data_file=os.path.join('..','data','house_tiny.csv')
+with open(data_file,'w') as f:
+    f.write('NumRooms,Alley,Price\n')
+    f.write('NA,Pave,127000\n')
+    f.write('2,NA,34671342\n')
+
+
+data=pd.read_csv(data_file)
+print(data)
